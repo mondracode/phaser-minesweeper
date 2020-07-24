d37489b0973b30b59d2df1dd4ea47735c345ee31
Santiago Mondragón Gómez <smondragon@unal.edu.co>
2020-07-23 20:57:17 -0500

    Fixed spritesheet

--- game.js
+++ game.js
@@ -2,15 +2,16 @@ let TitleScreen = new Phaser.Class({
     Extends: Phaser.Scene,
 
     initialize: function TitleScreen(){
-        Phaser.Scene.call(this, {key: 'titleScreen', active: true});
+        Phaser.Scene.call(this, {key: "titleScreen", active: true});
     },
 
     preload: function(){
+        this.cameras.main.setBackgroundColor("#C0C0C0");
         
     },
 
     create: function(){
-
+        this.add.text(20, 20, "Loading...");
     }
 })
 
@@ -18,15 +19,16 @@ let GameScreen = new Phaser.Class({
     Extends: Phaser.Scene,
 
     initialize: function GameScreen(){
-        Phaser.Scene.call(this, {key: 'gameScreen', active: true});
+        Phaser.Scene.call(this, {key: "gameScreen", active: true});
     },
 
     preload: function(){
-        
+        this.load.spritesheet("board", "assets/board.png", {frameWidth: 16, frameHeight: 16});
     },
 
     create: function(){
-
+        this.cell = this.add.image(10, 10, "board", 2);
+        this.cell.setInteractive();
     }
 })
 
